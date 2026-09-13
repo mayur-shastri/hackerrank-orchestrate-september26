d8c3c0b55034778c8f2ee4e2cef9479a5b403b1d
Mayur Shastri <shastrimayur2993@gmail.com>
2026-09-13 16:49:56 +0530

    Add

--- code/utils/llm.py
+++ code/utils/llm.py
@@ -10,6 +10,7 @@ def get_openai_client() ->  AsyncOpenAI:
     return  AsyncOpenAI(
         base_url=os.environ["LLM_BASE_URL"],
         api_key=os.environ["LLM_API_KEY"],
+         max_retries=0,
     )
 
 openai_client = get_openai_client()

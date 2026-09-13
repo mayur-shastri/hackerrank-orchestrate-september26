805ce0da941fa9c231354c26c8c2e009e8b411b8
Mayur Shastri <shastrimayur2993@gmail.com>
2026-09-13 17:22:14 +0530

    json

--- code/world_model/replay.py
+++ code/world_model/replay.py
@@ -240,6 +240,8 @@ Instructions:
                 "content": prompt,
             },
         ],
+        response_format={"type": "json_object"},
+        temperature=0,
     )
 
     content = response.choices[0].message.content

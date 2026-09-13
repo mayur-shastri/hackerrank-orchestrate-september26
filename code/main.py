a081b0a8741173b18b69af1cee9058960ffe7c12
Mayur Shastri <shastrimayur2993@gmail.com>
2026-09-13 17:54:17 +0530

    fdf

--- code/main.py
+++ code/main.py
@@ -29,7 +29,7 @@ def main():
             """
             SELECT user_id, request_date
             FROM requests
-            WHERE user_id = user_02
+            WHERE user_id = 'user_02'
             """
         ).fetchone()
 
